599b25c2e9f4a43e85cae7eae612603c6ec7a5bc
alvarorga <alvarorga@hotmail.com>
2019-09-10 23:46:36 +0200

    Update candidates after a site has been fixed

--- src/strategies.jl
+++ src/strategies.jl
@@ -2,6 +2,29 @@
 # https://www.learn-sudoku.com/basic-techniques.html
 
 """
+    update_cands!(s::Sudoku, i::Int, j::Int)
+
+Update candidates if position (i, j) was fixed.
+"""
+function update_cands!(s::Sudoku, i::Int, j::Int)
+    n = s.grid(i, j)
+    # Check same row.
+    for k=1:9
+        if (s.cands[g2ix(i, k)]>>(n-1))&1 == 1
+           s.cands[g2ix(i, k)] -= 1<<(n-1)
+       end
+    end
+    # Check same column.
+    for k=1:9
+        if (s.cands[g2ix(k, j)]>>(n-1))&1 == 1
+           s.cands[g2ix(k, j)] -= 1<<(n-1)
+       end
+    end
+    # Check same block.
+    return s
+end
+
+"""
     lone_singles!(s::Sudoku)
 
 Make fixed sites with only one candidate.
